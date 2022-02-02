59e769c32a523ebf3cd6bbf301658d5e831ea0d0
barviz <barbaravizgarra@gmail.com>
2022-02-02 16:00:25 -0300

    servidor y servicio de tareas listo

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,28 +1,3 @@
 import { Task } from "./Task";
 
-export const TASKS: Task[] = [
-  {
-    id: 1,
-    text: 'terminar primer modulo de angular',
-    day: 'febrero 2 a las 14:34',
-    reminder: true
-  },
-  {
-    id: 2,
-    text: 'terminar segundo modulo de angular',
-    day: 'marzo 2 a las 14:34',
-    reminder: true
-  },
-  {
-    id: 3,
-    text: 'terminar tercer modulo de angular',
-    day: 'abril 2 a las 14:34',
-    reminder: false
-  },
-  {
-    id: 4,
-    text: 'terminar cuarto modulo de angular',
-    day: 'mayo 2 a las 14:34',
-    reminder: false
-  }
-];
+export const TASKS: Task[] = []
---
+++ src/app/service/task.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from 'src/app/Task';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl = 'http://localhost:5001/tasks'
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]>{
+    //const tasks = of(TASKS);
+    //return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+}
